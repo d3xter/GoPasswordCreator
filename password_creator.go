c8fc93aa2c801e88c221a725101cde4cf7731c16
Andreas Sinz <andreas.sinz@aon.at>
2010-05-28 12:12:09 +0200

    Added LICENSE file and headers. GoPasswordCreator is now licensed under the GPLv2

--- password_creator.go
+++ password_creator.go
@@ -1,3 +1,15 @@
+/*Copyright (C) 2010 Andreas Sinz
+
+This file is part of GoPasswordCreator.
+
+GoPasswordCreator is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; only version 2 of the License.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 package password_creator
 
 import (
