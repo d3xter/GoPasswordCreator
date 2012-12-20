20fb19f29fe1a62108d6ebd47d44245011e90604
Luther <lutheroto@gmail.com>
2012-12-19 23:20:57 -0500

    Fix grammar in comments and help strings

--- main.go
+++ main.go
@@ -21,22 +21,22 @@ import (
 var (
 	passwordLength = flag.Int("length", 8, "Length of the generated Password")
 
-	//Define all available flags, which are used to specify, which characters to use to generate the password
+	//Define all available flags which are used to specify which characters to use to generate the password
 	lowerCase         = flag.Bool("lower", false, "Should LowerCase characters be included?")
 	upperCase         = flag.Bool("upper", false, "Should UpperCase characters be included?")
 	numerals          = flag.Bool("numbers", false, "Should the Numbers be included?")
 	specialCharacters = flag.Bool("special", false, "Should special characters be included?")
-	usersCharacters   = flag.String("own", "", "Characters defined by the user, which will be also be used to generate the password")
+	usersCharacters   = flag.String("own", "", "Characters defined by the user which will be also be used to generate the password")
 
-	//If --all Flag is set, then lower/upper-case letters, numbers and special characters and by user defined characters are used
-	allGroups = flag.Bool("all", false, "Use lower/upper-case letters, numbers, special characters and by user defined characters to generate the password")
+	//If --all Flag is set, then lower/upper-case letters, numbers, special characters, and user defined characters are used
+	allGroups = flag.Bool("all", false, "Use lower/upper-case letters, numbers, special characters, and user defined characters to generate the password")
 
-	//The user can determine, how many passwords will be created
-	passwordCount = flag.Int("count", 1, "Determine, how many passwords will be created")
+	//The user can determine how many passwords will be created
+	passwordCount = flag.Int("count", 1, "Determine how many passwords will be created")
 
-	//The user can define a file, where the passwords will be written into
-	//If file is omitted, then it will print the passwords on Stdout
-	file = flag.String("file", "", "The File, where the passwords should be written into")
+	//The user can define a file where the passwords will be written into.
+	//If file is omitted, then it will print the passwords on Stdout.
+	file = flag.String("file", "", "The File where the passwords should be written into")
 )
 
 
